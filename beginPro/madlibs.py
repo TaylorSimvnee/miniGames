5432175b8997d39e033fcdb8e61299248d462091
TaylorSimvnee <Mstaylorg98@gmail.com>
2022-05-04 14:31:24 -0700

    first commit

---
+++ beginPro/madlibs.py
@@ -0,0 +1,11 @@
+
+adj = input("Adjective: ")
+verb1= input("Verb: ")
+verb2= input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Traveling is absolutely {adj}! It really just amazes me how \
+time flies by while adventuring! I love to {verb1} while abroad! Smile, wave, and {verb2} like you \
+are {famous_person}!"
+
+print(madlib)
